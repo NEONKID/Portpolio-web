2baab1a834436d5f365f0ed26f413aa144a7a97b
N.K <clax1412@outlook.com>
2018-04-07 13:16:47 +0900

    Build only master branch...

--- src/components/Container/Primary/About/Content/index.js
+++ src/components/Container/Primary/About/Content/index.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AboutContent extends Component {
     render() {
         return (
-            <p>저는 Back-end Developer를 목표로 하고 있는 작은 개발자입니다.</p>
+            <p>저는 Back-end Developer를 목표로 하고 있는 개발자입니다.</p>
         );
     }
 }
