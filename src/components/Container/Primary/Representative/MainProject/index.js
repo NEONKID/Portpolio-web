a4020d942264cf47711ac28e4b1898958e828abc
N.K <clax1412@outlook.com>
2018-05-08 17:06:10 +0900

    Add HomeIoT Project PV...

--- src/components/Container/Primary/Representative/MainProject/index.js
+++ src/components/Container/Primary/Representative/MainProject/index.js
@@ -9,13 +9,19 @@ import HomeIoT from '../img/HomeIoT_Main.png';
 
 class MainProject extends Component {
     state = {
-        visible: false
+        isVisible: false
     }
 
-    openModal = () => this.setState({ visible: true })
-    closeModal = () => this.setState({ visible: false })
+    openModal = () => this.setState({ isVisible: true })
+    closeModal = () => this.setState({ isVisible: false })
 
     render() {
+        const modalView = (
+            <Modal visible={this.state.isVisible} width="900" height="800" effect="fadeInUp">
+                <Demo onClose={this.closeModal} />
+            </Modal>
+        );
+
         return (
             <div className="item featured">
                 <h3 className="title">HomeIoT</h3>
@@ -33,9 +39,7 @@ class MainProject extends Component {
                     <p>해당 작품에서 저는 Client Application을 담당하였으며 플랫폼으로 Android를 선택하였고, 집 안의 센서들을 버튼으로 제어하는 것과 동시에 음성으로 제어할 수 있도록 구현하였습니다.</p>
                 </div>        
                 <Button color="info" className="btn-cta-secondary" onClick={this.openModal}><FontAwesome name='thumbs-o-up' /> View Demo</Button>
-                <Modal visible={this.state.visible} width="1200" height="800" effect="fadeInUp">
-                    <Demo onClose={this.closeModal} />
-                </Modal>
+                { this.state.isVisible ? modalView : undefined }
             </div>
         );
     }
