6730ecd75a7573d1798b1efc7cd7cd9d86fcf6ce
N.K <clax1412@outlook.com>
2019-03-17 16:28:10 +0900

    Refactoring intl...

--- src/locale/ko/index.js
+++ src/locale/ko/index.js
@@ -1,6 +1,94 @@
-export default {
+const KOR = {
     ko: {
-        'hello': "Hello I'm Kwang Soo Jeong (Neon K.I.D)",
-        '': ''
-    }
-}
+        /**
+         * Header
+         * Todo: Translation
+         */
+
+        /**
+         * Primary Container (About)
+         */
+        'ab-card': '자기 소개',
+        'ab-intro': "Hello I'm Kwang Soo Jeong (Neon K.I.D)",
+
+        /**
+         * Primary Container (Representative)
+         */
+        'rp-card': '주요 프로젝트',
+        'rp-title': 'HomeIoT',
+        'rp-summary': 'Arduino, Windows Server, SmartPhone을 사용한 가정용 IoT 시스템',
+        'rp-main-desc-A': '집 안에 있는 창문, 현관문들이 온도와 기상 환경 데이터들의 변화를 통해 이들을 자동 제어하고 집 바깥에서도 제어할 수 있는 스마트홈을 생각하여 개발한 저의 졸업 작품입니다.',
+        'rp-main-desc-B': '해당 작품에서 저는 Client Application을 담당하였으며 플랫폼으로 Android를 선택하였고, 집 안의 센서들을 버튼으로 제어하는 것과 동시에 음성으로 제어할 수 있도록 구현하였습니다.',
+        'rp-sub-A-desc': 'Windows용 공개형 POS 프로그램',
+        'rp-sub-B-desc': 'Java기반 수입/지출 기반형 다이어리 프로그램',
+        'rp-sub-C-desc': 'Radiology-CDM에 기반한 방사선 이미지 뷰어',
+
+        /**
+         * Primary Container (CodeWall)
+         */
+        'op-title': '다른 프로젝트',
+        'op-A-desc': '단국대학교 천안캠퍼스 길 안내 애플리케이션',
+        'op-B-desc': '방사선 이미지 ETL 모듈 (OHDSI 협력 프로젝트)',
+        'op-C-desc': 'DLUG Chatbot (2019. 12. 31 서비스 종료 예정)',
+
+        /**
+         * Secondary Container (Location info)
+         */
+        'my-loc': '대한민국 천안',
+
+        /**
+         * Secondary Container (Technology Form)
+         */
+        'tech-title': '주요 역량',
+        'tech-summary': '사용해 본 언어/기술과 수준',
+        'tech-python-desc': 'Flask 를 사용하여 REST API 개발 경험 있음',
+        'tech-java-desc': 'Spring boot 개발, JavaFX 개발 및 배포',
+        'tech-r-desc': 'ETL 모듈 개발 경험 및 현재 오픈 프로젝트 기여 중',
+        'tech-linux-desc': '소규모 리눅스 서버 관리 경험 / 개발 운영체제로 사용 중',
+        'tech-android-desc': '안드로이드 앱 개발 경험 있음',
+        'tech-basic-grade': '초급',
+        'tech-mid-grade': '중급',
+        'tech-pro-grade': '고급',
+
+        /**
+         * Secondary Container (Education Form)
+         */
+        'edu-title': '학적',
+        'edu-hs-major': '정보처리과',
+        'edu-hs-name': '천안상업고등학교',
+        'edu-univ-major': '컴퓨터과학과',
+        'edu-univ-name': '단국대학교',
+
+        /**
+         * Secondary Container (Language Form)
+         */
+        'lang-title': '언어',
+        'lang-ko': '한국어',
+        'lang-jp': '일본어',
+        'lang-en': '영어',
+        'lang-speaker': '모국어',
+        'lang-speaking': '긴단한 의사소통 가능',
+        'lang-reading': '간단한 독해 가능',
+
+        /**
+         * Secondary Container (Conference Form)
+         */
+        'conf-title': '외부 활동',
+
+        /**
+         * Secondary Container (Blog Post)
+         */
+        'ba-title': '최근 블로그 글',
+
+        /**
+         * Button, link components
+         */
+        'more-github': '다른 프로젝트 보기 ',
+        'view-source': '소스 보기',
+        'find-more': '더 알아보기',
+        'os-label': '오픈 소스',
+        'my-github': 'Github 활동',
+    },
+};
+
+export default KOR;
