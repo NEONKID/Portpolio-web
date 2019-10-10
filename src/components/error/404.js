0ed0834ccb174d66fceffa37f476235c08225b4f
N.K <clax1412@outlook.com>
2019-10-10 17:53:14 +0900

    Removed legacy v1.0 code..

--- src/components/error/404.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const NotFound = ({ location }) => (
-    <div>
-        <h2>No match found for <code>{location.pathname}</code></h2>
-    </div>
-);
-
-export default NotFound;
--- src/locale/index.js
+++
@@ -1,4 +0,0 @@
-import en from './en';
-import ko from './ko';
-
-export default Object.assign(en, ko);
