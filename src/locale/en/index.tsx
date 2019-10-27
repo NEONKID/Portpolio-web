700715a564f54c1a78130cc8905b47b6f5a05c62
N.K <clax1412@outlook.com>
2019-10-27 20:36:14 +0900

    Add translation files...

---
+++ src/locale/en/index.tsx
@@ -0,0 +1,30 @@
+const ENG = {
+	en: {
+		'menu-resume': 'Resume',
+		'menu-portfolio': 'Portfolio',
+		'menu-blog': 'Blog',
+		'menu-contact': 'Contact',
+		'intro-title': "I'm a Back-end Developer. I aim to make a difference through my creative solution.",
+		'intro-desc':
+			'I worked with my colleagues on a project to develop a stable application. I aim to use my application in any situation.',
+		'name-title': 'Name',
+		name: 'Kwang Soo Jeong',
+		'birth-title': 'Date of birth',
+		birth: 'August 25, 1993',
+		'email-title': 'E-mail',
+		'exp-year': ' year experience',
+		'exp-years': ' years experience',
+		'con-name-form': 'Name*',
+		'con-email-form': 'E-mail*',
+		'con-desc-form': 'Your Message',
+		'con-name-error': 'Please enter a value with valid length',
+		'con-email-error': 'Please enter a valid email address',
+		'con-desc-error': 'Please enter a description',
+		'con-loc-title': 'Location',
+		'con-loc-content': 'Cheonan, Republic of KR',
+		'con-email-title': 'E-mail',
+		'con-send-btn': 'Send Message',
+	},
+};
+
+export default ENG;
