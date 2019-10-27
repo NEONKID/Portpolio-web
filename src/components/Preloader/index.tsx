12c870494189150d6940636dae98fa5a4ae80826
N.K <clax1412@outlook.com>
2019-10-27 21:13:05 +0900

    Change Splash screen....

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 
+import Favicon from './favicon.png';
+
 import '../../jq';
 import './styles.css';
 
 const Preloader = () => {
 	useEffect(() => {
-		// 브라우저에 페이지가 로드 되었을 때, 발생하는 이벤트..
-		// HTML5를 이용한 정의된 애니메이션을 실행,,
 		$(window).on('load', () => {
 			$('.preloader').fadeOut('slow', () => {
 				$('.preloader-left').addClass('slideOut-left');
@@ -18,7 +18,7 @@ const Preloader = () => {
 	return (
 		<div className="preloader">
 			<div className="anim pulse">
-				<i className="ion-ios-star"></i>
+				<img src={Favicon} alt="" />
 			</div>
 		</div>
 	);
