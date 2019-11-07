d0d69892c72d1d7c0e611727e3216d83e88f92bb
N.K <clax1412@outlook.com>
2019-11-07 09:56:27 +0900

    Translate japanese...

---
+++ src/locale/jp/index.tsx
@@ -0,0 +1,35 @@
+const JPN = {
+	jp: {
+		'menu-resume': '履歴書',
+		'menu-portfolio': 'ポートフォリオ',
+		'menu-blog': 'ブログ',
+		'menu-contact': 'コンタクト',
+		'intro-title': '様々なことを学び、作ってみることが好きなバックエンドの開発者。',
+		'intro-desc':
+			'安定したアプリケーションを開発するために、プロジェクトでは常に協力する姿を見せようと努力し、特に新しいサービスを開発して勉強することが好きで、より良いプログラムのロジックを実装し出すために努力します。',
+		'name-title': '名前',
+		name: '鄭 光 洙',
+		'birth-title': '誕生日',
+		birth: '1993年 08月 25日',
+		'email-title': 'メールアドレス',
+		'exp-year': '年のキャリア',
+		'exp-years': '年のキャリア',
+		'filter-all': '全体',
+		'filter-desktop': 'Desktop アプリ',
+		'filter-mobile': 'Mobile アプリ',
+		'filter-web': 'Web アプリ',
+		'filter-ml': '人工知能',
+		'con-name-form': '名前*',
+		'con-email-form': 'メールアドレス*',
+		'con-desc-form': 'メッセージを入力してください。',
+		'con-name-error': '名前を入力してください。',
+		'con-email-error': '有効なメールアドレスを入力してください。',
+		'con-desc-error': '連絡することを入力してください。',
+		'con-loc-title': '位置',
+		'con-loc-content': '大韓民国 天安市',
+		'con-email-title': 'メールアドレス',
+		'con-send-btn': '転送',
+	},
+};
+
+export default JPN;
