b1f417cbfb2a4a0d721e0fd43d97a85d89d3336b
NEONKID <clax1412@outlook.com>
2020-03-15 18:46:39 +0900

    Remove previous translate files..

--- src/components/Server/index.tsx
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/storage';
-
-import config from './config.json';
-
-const fb = firebase.initializeApp(config);
-
-export default fb;
--- src/locale/index.tsx
+++
@@ -1,5 +0,0 @@
-import en from './en';
-import jp from './jp';
-import ko from './ko';
-
-export default Object.assign(en, jp, ko);
