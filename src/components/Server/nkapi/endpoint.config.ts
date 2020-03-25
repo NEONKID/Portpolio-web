f816308373efd1cf4c4a6641a48d5f67d06ae71b
NEONKID <clax1412@outlook.com>
2020-03-25 12:32:06 +0900

    Refactoring blog content uri..

--- src/components/Server/nkapi/endpoint.config.ts
+++ src/components/Server/nkapi/endpoint.config.ts
@@ -1,7 +1,15 @@
+const CORS_PROXY = 'https://cors-anywhere.herokuapp.com';
+
+const BLOG_URL = 'https://blog.neonkid.xyz/rss';
 const SERVER = 'https://apis.neonkid.xyz';
-const API_PREFIX = 'api';
 
 interface Config {
+	blog: {
+		receive: {
+			content: string;
+		};
+	};
+
 	contact: {
 		send: {
 			message: string;
@@ -10,9 +18,15 @@ interface Config {
 }
 
 const config: Config = {
+	blog: {
+		receive: {
+			content: `${CORS_PROXY}/${BLOG_URL}`,
+		},
+	},
+
 	contact: {
 		send: {
-			message: `${SERVER}/${API_PREFIX}/v1/sendMessage`,
+			message: `${SERVER}/v1/sendMessage`,
 		},
 	},
 };
