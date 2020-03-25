affde3c096156923614ac3c4aa3854af96fe0ce2
NEONKID <clax1412@outlook.com>
2020-03-25 12:35:20 +0900

    Fix API Endpoint..

--- src/components/Server/nkapi/endpoint.config.ts
+++ src/components/Server/nkapi/endpoint.config.ts
@@ -2,6 +2,7 @@ const CORS_PROXY = 'https://cors-anywhere.herokuapp.com';
 
 const BLOG_URL = 'https://blog.neonkid.xyz/rss';
 const SERVER = 'https://apis.neonkid.xyz';
+const API_PREFIX = 'api';
 
 interface Config {
 	blog: {
@@ -26,7 +27,7 @@ const config: Config = {
 
 	contact: {
 		send: {
-			message: `${SERVER}/v1/sendMessage`,
+			message: `${SERVER}/${API_PREFIX}/v1/sendMessage`,
 		},
 	},
 };
