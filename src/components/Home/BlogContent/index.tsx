8f14b0a48f96dd549b983c1796eda4c9eaaa3515
NEONKID <clax1412@outlook.com>
2020-05-08 21:15:21 +0900

    Add blogcontent click handler, because too many request ..

--- src/components/Home/BlogContent/index.tsx
+++ src/components/Home/BlogContent/index.tsx
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
+import React, { useEffect, useState, ReactElement } from 'react';
+import { observer } from 'mobx-react';
 import { Item } from 'rss-parser';
+import { usePromiseTracker } from 'react-promise-tracker';
+import Loader from 'react-loader-spinner';
 
 import Pagination from './Pagination';
 import Post from './Post';
@@ -9,39 +11,29 @@ import { receivePost } from '../../Server/nkapi/receivePost';
 
 import * as Inject from '../../../stores/MenuStateStore';
 
-interface BCState {
-	data: any;
-	pageSize: number;
-	itemCnt: number;
-	curPage: number;
-}
+const useData = () => {
+	const { store } = Inject.useStores();
 
-interface BCProps {}
+	return {
+		blogState: store.blogState,
+	};
+};
 
-@inject((stores: Inject.Props) => ({
-	blogState: stores.store.blogState,
-}))
-@observer
-class BlogContent extends Component<BCProps, BCState> {
-	constructor(props: BCProps) {
-		super(props);
+const BlogContent = () => {
+	const data = useData();
 
-		this.state = {
-			data: [],
-			pageSize: 3,
-			curPage: 1,
-			itemCnt: 0,
-		};
-	}
+	const [feeds, setFeeds] = useState<Item[]>([]);
+	const [pageSize, setPageSize] = useState(3);
+	const [curPage, setCurPage] = useState(1);
+	const [itemCnt, setItemCnt] = useState(0);
 
-	componentDidMount() {
-		receivePost().then(feed => {
-			this.setState({ data: feed.items });
-			this.setState({ itemCnt: this.state.data.length });
-		});
-	}
+	const style = data.blogState ? 'content-blocks blog showx' : 'content-blocks blog';
 
-	prettyContent = (content: any) => {
+	const pageChange = (page: number) => {
+		setCurPage(page);
+	};
+
+	const prettyContent = (content: any) => {
 		if (typeof content === 'string') {
 			return content
 				.replace(/(<([^>]+)>)/gi, '')
@@ -51,50 +43,61 @@ class BlogContent extends Component<BCProps, BCState> {
 		} else return '';
 	};
 
-	hanldePageChange = (page: number) => {
-		this.setState({ curPage: page });
-	};
+	const { promiseInProgress } = usePromiseTracker();
 
-	render() {
-		const { blogState }: any = this.props;
-		const style = blogState ? 'content-blocks blog showx' : 'content-blocks blog';
-		const posts = paginate(this.state.data, this.state.curPage, this.state.pageSize);
+	useEffect(() => {
+		if (data.blogState) {
+			receivePost().then((feed: Item) => {
+				setFeeds(feed.items);
+				setItemCnt(feeds.length);
+			});
+		}
+	});
 
-		return (
-			<div className={style}>
-				<section className="content">
-					<div className="block-content">
-						<h3 className="block-title">N.K Lab</h3>
-						<div className="row">
-							<div className="col-md-10 mx-auto">
-								<div className="post">
-									{posts.map((item: Item, i: number) => {
-										let desc = this.prettyContent(item.content);
-										return (
-											<Post
-												title={item.title}
-												pubDate={item.pubDate}
-												categories={item.categories}
-												desc={desc}
-												address={item.guid}
-												key={i}
-											/>
-										);
-									})}
-								</div>
-								<Pagination
-									itemsCnt={this.state.itemCnt}
-									pageSize={this.state.pageSize}
-									curPage={this.state.curPage}
-									onPageChange={this.hanldePageChange}
-								/>
-							</div>
+	const posts = paginate(feeds, curPage, pageSize);
+
+	let curPost: ReactElement[] = [
+		<div className="text-center" style={{ paddingTop: '4em', paddingBottom: '4em' }}>
+			<Loader type="Grid" color="#727272" height={100} width={100}></Loader>
+		</div>,
+	];
+
+	if (!promiseInProgress) {
+		curPost = posts.map((item: Item, i: number) => {
+			let desc = prettyContent(item.content);
+			return (
+				<Post
+					title={item.title}
+					pubDate={item.pubDate}
+					categories={item.categories}
+					desc={desc}
+					address={item.guid}
+					key={i}
+				/>
+			);
+		});
+	}
+
+	return (
+		<div className={style}>
+			<section className="content">
+				<div className="block-content">
+					<h3 className="block-title">N.K Lab</h3>
+					<div className="row">
+						<div className="col-md-10 mx-auto">
+							<div className="post">{curPost}</div>
+							<Pagination
+								pageSize={pageSize}
+								itemsCnt={itemCnt}
+								curPage={curPage}
+								onPageChange={pageChange}
+							/>
 						</div>
 					</div>
-				</section>
-			</div>
-		);
-	}
-}
+				</div>
+			</section>
+		</div>
+	);
+};
 
-export default BlogContent;
+export default observer(BlogContent);
