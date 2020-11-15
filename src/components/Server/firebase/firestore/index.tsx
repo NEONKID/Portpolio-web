63ecccb310eb1f5a2082e23b2bbc581c4dfbb9c7
NEONKID <clax1412@outlook.com>
2020-11-15 10:47:34 +0900

    Fix firebase api type..

--- src/components/Server/firebase/firestore/index.tsx
+++ src/components/Server/firebase/firestore/index.tsx
@@ -1,10 +1,10 @@
-import { firestore } from 'firebase';
+import Firebase from 'firebase';
 
 import fb from '..';
 
 const db = fb.firestore();
 
-export const getEduInfo = (curLang: string): Promise<Array<firestore.QueryDocumentSnapshot>> => {
+export const getEduInfo = (curLang: string): Promise<Array<Firebase.firestore.QueryDocumentSnapshot>> => {
 	return new Promise((resolve, reject) => {
 		db.collection('edu')
 			.where('lang', '==', curLang)
@@ -15,7 +15,7 @@ export const getEduInfo = (curLang: string): Promise<Array<firestore.QueryDocume
 	});
 };
 
-export const getExp = (curLang: string): Promise<Array<firestore.QueryDocumentSnapshot>> => {
+export const getExp = (curLang: string): Promise<Array<Firebase.firestore.QueryDocumentSnapshot>> => {
 	return new Promise((resolve, reject) => {
 		db.collection('exp')
 			.where('lang', '==', curLang)
