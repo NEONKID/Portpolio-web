b055c0453b7bfd243533d0534f5d0262d3ecdade
NEONKID <contact@neonkid.xyz>
2021-09-19 21:19:19 +0900

    Remove old version src code..

--- src/components/Home/BlogContent/Pagination/paginate.ts
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-
-import { FeedEntry } from '../../../Server/nkapi/receivePost';
-
-export const paginate = (items: FeedEntry[], pageNumber: number, pageSize: number) => {
-	const startIdx = (pageNumber - 1) * pageSize;
-
-	return _(items).slice(startIdx).take(pageSize).value();
-};
